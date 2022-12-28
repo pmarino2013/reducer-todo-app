5d38f0b982171c868e6096f92ac4f7f64463967e
Pablo Marino <pmarino2013@gmail.com>
2022-12-28 12:01:48 -0300

    Template para comenzar con el proyecto

--- src/components/TaskCards.jsx
+++ src/components/TaskCards.jsx
@@ -1,40 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import "animate.css";
-const TaskCards = ({ item, deleteTarea, handleChange }) => {
-  const [activar, setActivar] = useState(false);
-  const [input, setInput] = useState(item.tarea);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    handleChange({ id: item.id, tarea: input.toUpperCase() });
-    setActivar(false);
-  };
+const TaskCards = () => {
   return (
-    <div
-      className="card my-3 animate__animated animate__fadeIn"
-      onDoubleClick={() => setActivar(!activar)}
-    >
+    <div className="card my-3 animate__animated animate__fadeIn">
       <div className="card-body d-flex align-items-center justify-content-between">
-        {activar ? (
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              className="form-control"
-              value={input.toUpperCase()}
-              onChange={(e) => setInput(e.target.value)}
-            />
-          </form>
-        ) : (
-          <span onTouchEnd={() => setActivar(!activar)}>
-            {input.toUpperCase()}
-          </span>
-        )}
-        <span
-          className="btn-delete text-white"
-          onClick={() => {
-            deleteTarea(item.id);
-          }}
-        >
+        <span>Tarea</span>
+        <span className="btn-delete text-white">
           <i className="fa fa-times" aria-hidden="true"></i>
         </span>
       </div>
