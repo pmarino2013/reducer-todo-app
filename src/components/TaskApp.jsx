009af0519fc221e973882ab4bdff637df9609a9c
Pablo Marino <pmarino2013@gmail.com>
2022-12-31 16:55:24 -0300

    Agregando sonido al cargar una tarea y al eliminarla

--- src/components/TaskApp.jsx
+++ src/components/TaskApp.jsx
@@ -1,10 +1,17 @@
 import React, { useReducer, useState } from "react";
 import { reducer } from "../reducer/talksReducer";
+
+import ReactAudioPlayer from "react-audio-player";
+import exclamacion from "../assets/exclamacion.mp3";
+
 import TaskCards from "./TaskCards";
 
 const TaskApp = () => {
   let inicialValue = [];
   const [textValue, setTextValue] = useState("");
+  const [play, setPlay] = useState(false);
+  const [sonidoDesmontar, setSonidoDesmontar] = useState(false);
+
   const [state, dispatch] = useReducer(reducer, inicialValue);
 
   const handleSubmit = (e) => {
@@ -13,6 +20,7 @@ const TaskApp = () => {
       type: "add",
       newTarea: textValue,
     });
+    setPlay(true);
     setTextValue("");
   };
 
@@ -21,6 +29,9 @@ const TaskApp = () => {
       type: "delete",
       tareaId: id,
     });
+    setTimeout(() => {
+      changeSonido(false);
+    }, 500);
   };
 
   const handleChange = (item) => {
@@ -29,33 +40,44 @@ const TaskApp = () => {
       tarea: item,
     });
   };
+
+  const changeSonido = (valor) => {
+    setSonidoDesmontar(valor);
+  };
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-12 col-md-6 offset-md-3">
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              value={textValue}
-              onChange={(e) => setTextValue(e.target.value)}
-              className="form-control"
-            />
-          </form>
+    <>
+      {sonidoDesmontar && (
+        <ReactAudioPlayer src={exclamacion} autoPlay={sonidoDesmontar} />
+      )}
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-6 offset-md-3">
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                value={textValue}
+                onChange={(e) => setTextValue(e.target.value)}
+                className="form-control"
+              />
+            </form>
+          </div>
+        </div>
+        <div className="row mt-3">
+          <div className="col-12 col-md-6 offset-md-3">
+            {state.map((item) => (
+              <TaskCards
+                key={item.id}
+                item={item}
+                deleteTarea={deleteTarea}
+                handleChange={handleChange}
+                play={play}
+                changeSonido={changeSonido}
+              />
+            ))}
+          </div>
         </div>
       </div>
-      <div className="row mt-3">
-        <div className="col-12 col-md-6 offset-md-3">
-          {state.map((item) => (
-            <TaskCards
-              key={item.id}
-              item={item}
-              deleteTarea={deleteTarea}
-              handleChange={handleChange}
-            />
-          ))}
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
--- src/components/TaskCards.jsx
+++ src/components/TaskCards.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
+import ReactAudioPlayer from "react-audio-player";
+
+import notificacion from "../assets/notificacion.mp3";
+
 import "animate.css";
-const TaskCards = ({ item, deleteTarea, handleChange }) => {
+
+const TaskCards = ({ item, deleteTarea, handleChange, play, changeSonido }) => {
   const [activar, setActivar] = useState(false);
   const [input, setInput] = useState(item.tarea);
 
@@ -10,35 +15,40 @@ const TaskCards = ({ item, deleteTarea, handleChange }) => {
     setActivar(false);
   };
   return (
-    <div
-      className="card my-3 animate__animated animate__fadeIn"
-      onDoubleClick={() => setActivar(!activar)}
-    >
-      <div className="card-body d-flex align-items-center justify-content-between">
-        {activar ? (
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              className="form-control"
-              value={input.toUpperCase()}
-              onChange={(e) => setInput(e.target.value)}
-            />
-          </form>
-        ) : (
-          <span onTouchEnd={() => setActivar(!activar)}>
-            {input.toUpperCase()}
+    <>
+      <ReactAudioPlayer src={notificacion} autoPlay={play} />
+
+      <div
+        className="card my-3 animate__animated animate__fadeIn"
+        onDoubleClick={() => setActivar(!activar)}
+      >
+        <div className="card-body d-flex align-items-center justify-content-between">
+          {activar ? (
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                className="form-control"
+                value={input.toUpperCase()}
+                onChange={(e) => setInput(e.target.value)}
+              />
+            </form>
+          ) : (
+            <span onTouchEnd={() => setActivar(!activar)}>
+              {input.toUpperCase()}
+            </span>
+          )}
+          <span
+            className="btn-delete text-white"
+            onClick={() => {
+              changeSonido(true);
+              deleteTarea(item.id);
+            }}
+          >
+            <i className="fa fa-times" aria-hidden="true"></i>
           </span>
-        )}
-        <span
-          className="btn-delete text-white"
-          onClick={() => {
-            deleteTarea(item.id);
-          }}
-        >
-          <i className="fa fa-times" aria-hidden="true"></i>
-        </span>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
